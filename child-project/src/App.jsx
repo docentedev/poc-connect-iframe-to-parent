ff9043643f3bd4bee441ff2ab52b495c6be02db8
Claudio Esteban Rojas Rodriguez <Claudio.rojas_ex@scotiabank.cl>
2020-04-13 15:53:40 -0400

    feat: added value counter

--- child-project/src/App.jsx
+++ child-project/src/App.jsx
@@ -4,13 +4,14 @@ import './App.css';
 function App() {
 
   const [modalResult, setModalResult] = useState(null);
+  const [counter, setCounter] = useState(0);
 
   const handlerModal = function () {
     window.parent.postMessage({
       type: 'ui-modal__send',
       payload: {
         css: '* {color: red;} p {color: blue;} button {color:green;}',
-        content: '<p>Esta seguro que desea hacer esto?<p>',
+        content: `<p>Esta seguro que desea hacer esto por ${counter} vez?<p>`,
       }
     }, "*")
   };
@@ -20,6 +21,7 @@ function App() {
   function receiveMessage(event) {
     if (event.data.type === 'ui-modal__receive') {
       setModalResult(event.data.payload);
+      setCounter(counter + 1)
     }
   }
 
